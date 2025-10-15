4d7c9b6b9b490354ea1cf86af897eeb4e7d4bb9c
Maihos <monteirojuninho@hotmail.com>
2025-10-15 16:06:36 -0300

    feat: adicionar interfaces DTO para paginação e resposta

--- src/common/dto.ts
+++ src/common/dto.ts
@@ -1,1 +1,24 @@
 // Common DTOs - TODO: DTOs compartilhados
+
+// Este arquivo será usado para DTOs que são compartilhados entre módulos
+// Por enquanto está vazio, mas pode ser expandido conforme necessário
+
+export interface PaginationDto {
+  page?: number;
+  limit?: number;
+}
+
+export interface ResponseDto<T> {
+  data: T;
+  message?: string;
+  statusCode: number;
+}
+
+export interface PaginatedResponseDto<T> extends ResponseDto<T[]> {
+  pagination: {
+    page: number;
+    limit: number;
+    total: number;
+    totalPages: number;
+  };
+}
