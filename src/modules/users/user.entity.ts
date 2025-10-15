d2b268cda0203aaeb9ca7e63df31cc1b176eb94b
Maihos <monteirojuninho@hotmail.com>
2025-10-15 10:12:45 -0300

    feat: adicionar módulo e entidade de usuário com roles

--- src/modules/users/user.entity.ts
+++ src/modules/users/user.entity.ts
@@ -1,1 +1,48 @@
-// User Entity - TODO: Modelo de usuário
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+
+export enum UserRole {
+  ADMIN = 'admin',
+  OPERATOR = 'operator',
+  SUPERVISOR = 'supervisor',
+}
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  name: string;
+
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    default: UserRole.OPERATOR,
+  })
+  role: UserRole;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date | null;
+}
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,1 +1,12 @@
 // Users Module - TODO: Gerenciamento de usuários
+import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+
+@Module({
+  imports: [],
+  controllers: [UsersController],
+  providers: [UsersService],
+  exports: [UsersService],
+})
+export class UsersModule {}
