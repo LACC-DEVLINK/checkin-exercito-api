90f71935800ec7a78aa9ad5c903dd8f9fe928a21
rafaelgoesti <rafaelgoesti2021@gmail.com>
2025-10-18 13:36:49 -0300

    fix: remover TypeORM e usar apenas Prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 // src/app.module.ts
 import { Module, Global } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './common/prisma.service';
-import { QrCode } from './qr-codes/entities/qr-code.entity';
-import { QrCodesModule } from './qr-codes/qr-codes.module';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 
@@ -14,21 +11,8 @@ import { AuthModule } from './modules/auth/auth.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: configService.get('DB_TYPE') as any,
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_DATABASE'),
-        entities: [QrCode],
-        synchronize: true,
-      }),
-    }),
-    QrCodesModule,
+    // TODO: QrCodesModule precisa ser migrado para Prisma
+    // QrCodesModule,
     UsersModule,
     AuthModule,
   ],
